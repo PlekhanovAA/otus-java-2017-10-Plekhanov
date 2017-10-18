0f9e3ce11c4a0b5a6d0389b4e3f5dfcbda9c8e8c
PlekhanovAA <repn@yandex.ru>
2017-10-18 20:32:55 +0500

    add using external jar

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import net.time4j.Moment;
+import net.time4j.SystemClock;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Moment now = SystemClock.currentMoment();
+        System.out.println(now);
+    }
+}
