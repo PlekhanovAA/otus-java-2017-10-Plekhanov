5b074ed19925a6050ae8f6d4517c29e4f5fccf6f
PlekhanovAA <repn@yandex.ru>
2017-10-24 21:26:29 +0300

    HomeWork_2

--- HomeWork_2/src/main/java/Aleksey/Plekhanov/Main.java
+++ HomeWork_2/src/main/java/Aleksey/Plekhanov/Main.java
@@ -1,40 +1,59 @@
 package Aleksey.Plekhanov;
 
-import java.lang.management.ManagementFactory;
-
-/**
-* Runtime runtime = Runtime.getRuntime();
-        * long mem = runtime.totalMemory() - runtime.freeMemory();
-        * System.gc()
- */
-
 @SuppressWarnings({"RedundantStringConstructorCall", "InfiniteLoopStatement"})
 public class Main {
-    public static void main(String... args) throws InterruptedException {
-        System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
-        Runtime runtime = Runtime.getRuntime();
-        int size = 20_000_000;
 
-        System.out.println("Starting the loop");
-        while (true) {
-            long mem = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println("Memory  after array: " + mem);
-            Object[] array = new Object[size];
-            System.out.println("New array of size: " + array.length + " created");
-            for (int i = 0; i < size; i++) {
-                array[i] = new Object();
-                //array[i] = new String(""); //String pool
-                //array[i] = new String(new char[0]); //without String pool
-                //array[i] = new MyClass();
-            }
-            System.out.println("Created " + size + " objects.");
-            mem = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println("Memory  before array after GC: " + mem);
-            System.gc();
-            mem = runtime.totalMemory() - runtime.freeMemory();
-            System.out.println("Memory  before array before GC: " + mem);
-            Thread.sleep(1000); //wait for 1 sec
+    public static void main(String[] args) throws InterruptedException {
+        Runtime runtime = Runtime.getRuntime();
+        int size = 10_000_000;
+
+        Object[] array = new Object[size];
+        System.out.println("New array Object of size: " + array.length + " created");
+        System.gc();
+        long mem1 = runtime.totalMemory() - runtime.freeMemory();
+        for (int i = 0; i < size; i++) {
+            array[i] = new Object();
         }
+        System.out.println("Created " + size + " objects.");
+        System.gc();
+        long mem2 = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memory test: " + Tester.test(size, mem1, mem2));
+
+        array = new Object[size];
+        System.out.println("New array string pool of size: " + array.length + " created");
+        System.gc();
+        mem1 = runtime.totalMemory() - runtime.freeMemory();
+        for (int i = 0; i < size; i++) {
+            array[i] = new String(""); //String pool
+        }
+        System.out.println("Created " + size + " objects.");
+        System.gc();
+        mem2 = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memory test: " + Tester.test(size, mem1, mem2));
+
+        array = new Object[size];
+        System.out.println("New array without string pool of size: " + array.length + " created");
+        System.gc();
+        mem1 = runtime.totalMemory() - runtime.freeMemory();
+        for (int i = 0; i < size; i++) {
+            array[i] = new String(new char[0]); //without String pool
+        }
+        System.out.println("Created " + size + " objects.");
+        System.gc();
+        mem2 = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memory test: " + Tester.test(size, mem1, mem2));
+
+        array = new Object[size];
+        System.out.println("New array MyClass of size: " + array.length + " created");
+        System.gc();
+        mem1 = runtime.totalMemory() - runtime.freeMemory();
+        for (int i = 0; i < size; i++) {
+            array[i] = new MyClass();
+        }
+        System.out.println("Created " + size + " objects.");
+        System.gc();
+        mem2 = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memory test: " + Tester.test(size, mem1, mem2));
     }
 
     private static class MyClass {
--- HomeWork_2/src/main/java/Aleksey/Plekhanov/Tester.java
+++ HomeWork_2/src/main/java/Aleksey/Plekhanov/Tester.java
@@ -1,4 +1,8 @@
 package Aleksey.Plekhanov;
 
 public class Tester {
+
+    public static long test (int size, long mem1, long mem2) {
+        return (mem2 - mem1)/size;
+    }
 }
