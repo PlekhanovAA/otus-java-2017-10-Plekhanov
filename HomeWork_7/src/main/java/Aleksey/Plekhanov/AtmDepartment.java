4caf2b158181552cf90e3bd334150b0540fd3741
PlekhanovAA <repn@yandex.ru>
2017-12-07 21:25:51 +0300

    consideration of comments

--- HomeWork_7/src/main/java/Aleksey/Plekhanov/AtmDepartment.java
+++ HomeWork_7/src/main/java/Aleksey/Plekhanov/AtmDepartment.java
@@ -8,7 +8,7 @@ class AtmDepartment {
         boolean  proceed = true;
         try (Scanner console = new Scanner(System.in)) {
             while (proceed) {
-                AtmDepartmentService.printMenu();
+                printMenu();
                 int choice = console.nextInt();
                 switch (choice) {
                     case 1:
@@ -34,5 +34,15 @@ class AtmDepartment {
                 }
             }
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("---------- ATM DEPARTMENT MENU ---------");
+        System.out.println("1. Build ATM;");
+        System.out.println("2. Views ATM;");
+        System.out.println("3. Working with current ATM;");
+        System.out.println("4. Total volume;");
+        System.out.println("5. Revert state all ATMs;");
+        System.out.println("6. Exit.");
     }
 }
--- HomeWork_7/src/main/java/Aleksey/Plekhanov/AtmDepartmentService.java
+++ HomeWork_7/src/main/java/Aleksey/Plekhanov/AtmDepartmentService.java
@@ -8,16 +8,6 @@ class AtmDepartmentService {
 
     private static List<Atm> atms = new ArrayList<>();
 
-    static void printMenu() {
-        System.out.println("---------- ATM DEPARTMENT MENU ---------");
-        System.out.println("1. Build ATM;");
-        System.out.println("2. Views ATM;");
-        System.out.println("3. Working with current ATM;");
-        System.out.println("4. Total volume;");
-        System.out.println("5. Revert state all ATMs;");
-        System.out.println("6. Exit.");
-    }
-
     static void buildAtm(Scanner console) {
         System.out.println("Count banknotes: ");
         atms.add(new Atm(console.nextInt()));
@@ -47,7 +37,6 @@ class AtmDepartmentService {
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Incorrect choice");
             }
-
         }
     }
 
